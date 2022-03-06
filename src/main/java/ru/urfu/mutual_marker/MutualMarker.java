5bf12df1ee84d98afde30faf4ff018d629fed968
Iulia Chebykina <julia.chebykina.13@gmail.com>
2022-03-06 15:02:25 +0500

    initial commit

---
+++ src/main/java/ru/urfu/mutual_marker/MutualMarker.java
@@ -0,0 +1,13 @@
+package ru.urfu.mutual_marker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MutualMarker {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MutualMarker.class, args);
+    }
+
+}
---
+++ src/test/java/ru/urfu/mitial_marker/MutualMerkerTests.java
@@ -0,0 +1,13 @@
+package ru.urfu.mitial_marker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MutualMerkerTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
