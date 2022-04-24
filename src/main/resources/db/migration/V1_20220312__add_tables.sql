4010e04a2aafe81d36910128b61bc6410316bbf7
Iulia Chebykina <julia.chebykina.13@gmail.com>
2022-04-24 16:09:41 +0500

    add docker

--- src/main/resources/db/migration/V1_20220312__add_tables.sql
+++ src/main/resources/db/migration/V1_20220312__add_tables.sql
@@ -2,7 +2,6 @@ create schema if not exists mutual_marker;
 
 CREATE SEQUENCE if not exists mutual_marker.sequence start 100;
 
-
 CREATE TABLE mutual_marker.attachment
 (
     id         BIGINT NOT NULL,
