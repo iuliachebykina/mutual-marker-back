1b8877260aaa832a7e68114d3af2546e0e4a2d16
Iulia Chebykina <julia.chebykina.13@gmail.com>
2022-05-01 15:30:57 +0500

    fix login

--- src/main/java/ru/urfu/mutual_marker/security/SecurityConfigurerAdapter.java
+++ src/main/java/ru/urfu/mutual_marker/security/SecurityConfigurerAdapter.java
@@ -9,10 +9,17 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import ru.urfu.mutual_marker.jpa.entity.value_type.Role;
 
-import static org.springframework.security.config.Customizer.withDefaults;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 
 @Configuration
@@ -50,8 +57,37 @@ public class SecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
                 .csrf().disable()
                 .httpBasic()
                 .and().sessionManagement().disable()
-                .formLogin(withDefaults())
-                .logout(withDefaults());
+                .formLogin()
+                .loginProcessingUrl("/api/login")
+                .successHandler(successHandler())
+                .failureHandler(failureHandler())
+                .and()
+                .logout()
+                .logoutUrl("/api/logout");
+    }
+
+    private AuthenticationSuccessHandler successHandler() {
+        return new AuthenticationSuccessHandler() {
+            @Override
+            public void onAuthenticationSuccess(HttpServletRequest httpServletRequest,
+                                                HttpServletResponse httpServletResponse, Authentication authentication)
+                    throws IOException, ServletException {
+                httpServletResponse.getWriter().append("OK");
+                httpServletResponse.setStatus(200);
+            }
+        };
+    }
+
+    private AuthenticationFailureHandler failureHandler() {
+        return new AuthenticationFailureHandler() {
+            @Override
+            public void onAuthenticationFailure(HttpServletRequest httpServletRequest,
+                                                HttpServletResponse httpServletResponse, AuthenticationException e)
+                    throws IOException, ServletException {
+                httpServletResponse.getWriter().append("Authentication failure");
+                httpServletResponse.setStatus(401);
+            }
+        };
     }
 
 }
