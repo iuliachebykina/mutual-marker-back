ecbb8e573c5be94ed6b5d243ac9b6a6a232601b5
Iulia Chebykina <julia.chebykina.13@gmail.com>
2022-05-11 18:46:04 +0500

    fix build

--- src/main/java/ru/urfu/mutual_marker/api/MarksApi.java
+++ src/main/java/ru/urfu/mutual_marker/api/MarksApi.java
@@ -85,10 +85,6 @@ public class MarksApi {
 
     @GetMapping("/{projectId}")
     public ResponseEntity<Object> getAllMarksForProject(@PathVariable Long projectId){
-        try{
-            return new ResponseEntity<>(markService.getAllMarksForProject(projectId), HttpStatus.OK);
-        } catch (MarkServiceException e){
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+        return new ResponseEntity<>(markService.getAllMarksForProject(projectId), HttpStatus.OK);
     }
 }
--- src/main/java/ru/urfu/mutual_marker/service/exception/NotFoundException.java
+++ src/main/java/ru/urfu/mutual_marker/service/exception/NotFoundException.java
@@ -3,7 +3,6 @@ package ru.urfu.mutual_marker.service.exception;
 import ru.urfu.mutual_marker.exception.MutualMarkerApiException;
 
 public class NotFoundException extends MutualMarkerApiException {
-
     public NotFoundException(String reason) {
         super(reason);
     }
