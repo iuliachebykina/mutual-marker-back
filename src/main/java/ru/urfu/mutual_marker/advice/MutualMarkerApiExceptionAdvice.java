9f920dce04ac08ced6f2f67c5e9ad7282480306d
Iulia Chebykina <julia.chebykina.13@gmail.com>
2022-05-15 11:25:34 +0500

    catch error

--- src/main/java/ru/urfu/mutual_marker/advice/MutualMarkerApiExceptionAdvice.java
+++ src/main/java/ru/urfu/mutual_marker/advice/MutualMarkerApiExceptionAdvice.java
@@ -4,12 +4,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import ru.urfu.mutual_marker.exception.MutualMarkerApiException;
 
 @RestControllerAdvice
 public class MutualMarkerApiExceptionAdvice {
 
-    @ExceptionHandler(MutualMarkerApiException.class)
+    @ExceptionHandler(Exception.class)
     public ResponseEntity<?> mutualMarkerApiExceptionHandler(Throwable ex) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
     }
