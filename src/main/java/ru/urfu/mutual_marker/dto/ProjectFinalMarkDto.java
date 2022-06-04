a5c9db52bcdfc8c6fff5ef3444f965b5c2e5cf8c
MehLater <Artem.Zavialov@urfu.me>
2022-06-04 22:18:08 +0500

    Added schema to method to get all final marks for task

--- src/main/java/ru/urfu/mutual_marker/dto/ProjectFinalMarkDto.java
+++ src/main/java/ru/urfu/mutual_marker/dto/ProjectFinalMarkDto.java
@@ -1,5 +1,6 @@
 package ru.urfu.mutual_marker.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Data;
@@ -10,9 +11,14 @@ import ru.urfu.mutual_marker.jpa.entity.value_type.Name;
 @FieldDefaults(level = AccessLevel.PRIVATE)
 @Builder(toBuilder = true)
 public class ProjectFinalMarkDto {
+    @Schema(name = "Имя студента")
     Name studentName;
+    @Schema(name = "Название проекта")
     String projectTitle;
+    @Schema(name = "Id проекта")
     Long projectId;
+    @Schema(name = "Id профиля студента, которому принадлежит оценка")
     Long profileId;
+    @Schema(name = "Финальная оценка с точностью до 2 цифр после запятой, рассчитывается повторно при повторных запросах")
     Double finalMark;
 }
