e8cea8c238f701a22c74ce04af67debd8fba6f82
Iulia Chebykina <julia.chebykina.13@gmail.com>
2022-10-02 14:17:03 +0500

    refactoring

--- src/main/java/ru/urfu/mutual_marker/api/ProfileApi.java
+++ src/main/java/ru/urfu/mutual_marker/api/ProfileApi.java
@@ -167,7 +167,7 @@ public class ProfileApi {
 
     @Operation(summary = "Получение всех студентов в комнате по id комнаты")
     @GetMapping("/room/students/{roomId}")
-    //@PreAuthorize("@roomAccessEvaluator.isMemberOfRoomById(#roomId) or hasRole('ROLE_ADMIN')")
+    @PreAuthorize("@roomAccessEvaluator.isMemberOfRoomById(#roomId) or hasRole('ROLE_ADMIN')")
     public List<StudentInfo> getStudentsInRoom(@PathVariable Long roomId, @RequestParam("page") int page,
                                            @RequestParam("size") int size){
         Pageable pageable = PageRequest.of(page, size);
