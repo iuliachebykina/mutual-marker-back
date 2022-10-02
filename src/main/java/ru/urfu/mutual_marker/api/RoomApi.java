cc64fcb880034b8251c8b25605d35eea6fd96f53
Iulia Chebykina <julia.chebykina.13@gmail.com>
2022-10-02 15:35:01 +0500

    refactoring

--- src/main/java/ru/urfu/mutual_marker/api/RoomApi.java
+++ src/main/java/ru/urfu/mutual_marker/api/RoomApi.java
@@ -70,7 +70,7 @@ public class RoomApi {
 
     @Operation(summary = "Обновление имеющейся комнаты")
     @PutMapping("/room")
-    @PreAuthorize("(hasAnyRole('ROLE_ADMIN', 'ROLE_TEACHER') and @roomAccessEvaluator.isMemberOfRoomById(#roomId)) or hasRole('ROLE_ADMIN')")
+    @PreAuthorize("(hasAnyRole('ROLE_ADMIN', 'ROLE_TEACHER') and @roomAccessEvaluator.isMemberOfRoomByRoomCode(#room.getCode())) or hasRole('ROLE_ADMIN')")
     public ResponseEntity<Room> updateRoom(@RequestBody Room room){
         return new ResponseEntity<>(roomService.updateRoom(room), HttpStatus.OK);
 
