bb4f4aa41b3827bb6effa3f983f9f367a5c1d11b
Iulia Chebykina <julia.chebykina.13@gmail.com>
2022-10-02 14:15:09 +0500

    refactoring

--- src/main/java/ru/urfu/mutual_marker/exception/MutualMarkerApiException.java
+++ src/main/java/ru/urfu/mutual_marker/exception/MutualMarkerApiException.java
@@ -1,6 +1,6 @@
 package ru.urfu.mutual_marker.exception;
 
-public class MutualMarkerApiException extends Exception {
+public class MutualMarkerApiException extends RuntimeException {
     public MutualMarkerApiException(String message) {
         super(message);
     }
