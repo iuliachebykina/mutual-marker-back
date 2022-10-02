5d506f15fbd9ce685958341fd910cfc96b3f4dde
Iulia Chebykina <julia.chebykina.13@gmail.com>
2022-10-02 16:23:36 +0500

    refactoring

--- src/main/java/ru/urfu/mutual_marker/MutualMarker.java
+++ src/main/java/ru/urfu/mutual_marker/MutualMarker.java
@@ -1,13 +1,9 @@
 package ru.urfu.mutual_marker;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-
-@OpenAPIDefinition(servers = {@Server(url = "http://localhost:8080", description = "Default Server URL")})
 
 @SpringBootApplication
 @EnableJpaAuditing
